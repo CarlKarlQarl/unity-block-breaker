cb38d7f59200665735593ff129bae862f1f6e830
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-20 17:10:28 -0600

    Ball will launch on click/left click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
 
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start(){
@@ -14,7 +17,21 @@ public class Ball : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
+        if(!hasStarted){
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle(){
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick(){
+        if(Input.GetMouseButtonDown(0)){
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
     }
 }

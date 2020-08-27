b2fa6e85646db05fb86a9d50168e0ad9acee3af5
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-27 16:53:24 -0600

    Added sound to when blocks are destroyed, sound plays at point so the clip isn't destroyed with the block.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour{
+
+    [SerializeField] AudioClip breakSound;
+
     // Start is called before the first frame update
     void Start(){}
     // Update is called once per frame
     void Update(){}
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }

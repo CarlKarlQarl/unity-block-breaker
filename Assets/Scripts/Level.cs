524683212b81c9bf67f1db69b185856af7d92fe5
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-28 15:38:47 -0600

    Block destructions now decrease a counter. When the counter reaches zero, the scene changes to the next level.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Level : MonoBehaviour {
 
     [SerializeField] int breakableBlocks; //Serialized for debugging
+    SceneLoader sceneLoader;
 
     // Start is called before the first frame update
-    void Start(){}
+    void Start(){
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
     // Update is called once per frame
     void Update(){}
 
     public void CountBreakableBlocks(){
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed(){
+        breakableBlocks--;
+        if (breakableBlocks <= 0){
+            sceneLoader.LoadNextScene();
+        }
     }
 }

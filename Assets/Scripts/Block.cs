ab801898a2ccaf1d784750059f3ab29ef087b092
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-28 18:07:33 -0600

    Set-up GameStatus to keep track of score, when a block is destoryed, it calls a public function in GameStatus to increase the score by a set amount.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,10 +6,12 @@ public class Block : MonoBehaviour{
 
     [SerializeField] AudioClip breakSound;
     Level level;
+    GameStatus gameStatus;
 
     // Start is called before the first frame update
     void Start(){
         level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
         level.CountBreakableBlocks();
     }
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Block : MonoBehaviour{
     private void DestroyBlock(){
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         level.BlockDestroyed();
+        gameStatus.AddToScore();
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class GameStatus : MonoBehaviour{
 
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 10;
+
+    [SerializeField] int currentScore = 0; //Serialized for debugging
 
     // Start is called before the first frame update
     void Start(){}
     // Update is called once per frame
     void Update(){
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore(){
+        currentScore = currentScore + pointsPerBlockDestroyed;
     }
 }

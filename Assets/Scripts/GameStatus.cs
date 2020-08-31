9babdccb715e0fc72999ffcc850701b1080040f0
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-31 17:22:49 -0600

    Added UI text field to canvas and prefab, score now displays, initializes at start, and updates with each block being broken.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour{
 
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0; //Serialized for debugging
 
     // Start is called before the first frame update
-    void Start(){}
+    void Start(){
+        scoreText.text = "Score: " + currentScore;
+    }
     // Update is called once per frame
     void Update(){
         Time.timeScale = gameSpeed;
     }
 
     public void AddToScore(){
-        currentScore = currentScore + pointsPerBlockDestroyed;
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = "Score: " + currentScore;
     }
 }
